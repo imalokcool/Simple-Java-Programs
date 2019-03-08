ee33d61a0fe392dbae0ce49338d7db348f3afc51
Alok <mail2alok007@gmail.com>
2019-03-08 17:54:25 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+	public static void main(String args[])
+	{
+		int a;
+		Scanner s = new Scanner( System.in);
+		a = s.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("HELLO");
+		}
+}
+}
