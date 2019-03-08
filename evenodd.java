90a90d9133d84e7ddb97afc7b1d8126d6477906b
Alok <mail2alok007@gmail.com>
2019-03-08 11:26:39 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -1,9 +1,15 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-class evenodd
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
 {
-  public static void main (String args[])
-  {
-  Scanner s = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner s = new Scanner(System.in);
   int n = s.nextInt();
   if(n%2==0)
   {
@@ -17,5 +23,5 @@ class evenodd
    {
     System.out.print("Invalid");
    }
-}
+	}
 }
