130c8f858ab8932c17b0c584b340889ba1b1806b
Alok <mail2alok007@gmail.com>
2019-03-09 18:10:50 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,19 @@
+import java.math.*;
+import java.util.*;
+class factorial
+{
+
+    public static void main(String[] args) 
+    {
+
+        int n, sum=1;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        for(int i=1;i<=5;i++)
+        {
+        	sum  = n * i;
+        	System.out.println(sum);
+        }
+        
+    }
+}
