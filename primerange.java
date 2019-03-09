3ddd518d908e54169c45df9a19d2b325741e5856
Alok <mail2alok007@gmail.com>
2019-03-09 16:51:02 +0530

    Create primerange.java

---
+++ primerange.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class primerange
+{
+    public static void main(String args[])
+    {
+        int start, end, i, j, count=0;
+
+        Scanner scan = new Scanner(System.in);
+        start = scan.nextInt();
+        end = scan.nextInt();
+        for(i=start; i<=end; i++)
+        {
+            count = 0;
+            for(j=2; j<i; j++)
+            {
+                if(i%j == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+            if(count == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
