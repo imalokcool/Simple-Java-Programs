603e51c9f54488247f115a516f2b3976f566dd8a
Alok <mail2alok007@gmail.com>
2019-03-09 08:28:42 +0530

    Create oddseries.java

---
+++ oddseries.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class oddseries
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b;
+	Scanner s = new Scanner(System.in);
+	a = s.nextInt();
+	b = s.nextInt();
+	for(int i=a;i<b;i++)
+	{
+		if(i%2!=0)
+		{
+			System.out.print(i + " ");
+		}
+	}
+	}
+}
