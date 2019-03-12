9144fd37e07fc2b2f06fa501699a43adb095da22
Alok <mail2alok007@gmail.com>
2019-03-12 17:50:58 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class time
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		int n = s.nextInt();
+		int hr,min;
+		hr=n/60;
+		min=n%60;
+		System.out.print(hr + " " +min);
+	}
+}
